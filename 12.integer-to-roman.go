e1d9e0f353519a77569ed1b49bc8f6e40aa9fa12
notchips <2317853321@qq.com>
2019-07-14 14:32:03 +0800

    add 12, 13

--- 12.integer-to-roman.go
+++ 12.integer-to-roman.go
@@ -3,7 +3,62 @@
  *
  * [12] Integer to Roman
  */
+/*
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+*/
 func intToRoman(num int) string {
-    
+	buff := make([]byte, 0, 20)
+	for i := 1000; i > 0; i /= 10 {
+		n := num / i
+		num %= i
+		switch i {
+		case 1:
+			singleToRoman(&buff, 'I', 'V', 'X', n)
+		case 10:
+			singleToRoman(&buff, 'X', 'L', 'C', n)
+		case 100:
+			singleToRoman(&buff, 'C', 'D', 'M', n)
+		case 1000:
+			singleToRoman(&buff, 'M', '-', '-', n)
+		}
+	}
+	return string(buff)
 }
+
+/*
+a b c
+I V X
+X L C
+C D M
+M - -
+*/
+func singleToRoman(buff *[]byte, a, b, c byte, n int) {
+	switch {
+	case 1 <= n && n <= 3:
+		addChar(buff, a, n)
+	case n == 4:
+		addChar(buff, a, 1)
+		addChar(buff, b, 1)
+	case 5 <= n && n <= 8:
+		addChar(buff, b, 1)
+		addChar(buff, a, n-5)
+	case n == 9:
+		addChar(buff, a, 1)
+		addChar(buff, c, 1)
+	}
+}
+
+func addChar(buff *[]byte, char byte, n int) {
+	for i := 0; i < n; i++ {
+		*buff = append(*buff, char)
+	}
+}
+
 
---
+++ 13.roman-to-integer.go
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=13 lang=golang
+ *
+ * [13] Roman to Integer
+ */
+ func romanToInt(s string) int {
+	ret := 0
+	for i := 0; i < len(s); i++ {
+		ret += singleToInt(s[i])
+		if i > 0 {
+			switch {
+			case s[i-1] == 'I' && (s[i] == 'V' || s[i] == 'X'):
+				ret -= 2
+			case s[i-1] == 'X' && (s[i] == 'L' || s[i] == 'C'):
+				ret -= 20
+			case s[i-1] == 'C' && (s[i] == 'D' || s[i] == 'M'):
+				ret -= 200
+			}
+		}
+	}
+	return ret
+}
+
+func singleToInt(c byte) int {
+	switch c {
+	case 'I':
+		return 1
+	case 'V':
+		return 5
+	case 'X':
+		return 10
+	case 'L':
+		return 50
+	case 'C':
+		return 100
+	case 'D':
+		return 500
+	case 'M':
+		return 1000
+	default:
+		return 0
+	}
+}
+
