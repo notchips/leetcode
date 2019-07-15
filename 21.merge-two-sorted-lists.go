a4e5a2d7a6ec91acb40b0c2120b942ddf456ceab
notchips <2317853321@qq.com>
2019-07-15 13:52:55 +0800

    add 21

---
+++ 21.merge-two-sorted-lists.go
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=21 lang=golang
+ *
+ * [21] Merge Two Sorted Lists
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	var head = &ListNode{0, nil}
+	temp := head
+	for l1 != nil && l2 != nil {
+		if l2.Val < l1.Val {
+			temp.Next = l2
+			temp = temp.Next
+			l2 = l2.Next
+		} else {
+			temp.Next = l1
+			temp = temp.Next
+			l1 = l1.Next
+		}
+	}
+	if l1 != nil {
+		temp.Next = l1
+	}
+	if l2 != nil {
+		temp.Next = l2
+	}
+	return head.Next
+}
