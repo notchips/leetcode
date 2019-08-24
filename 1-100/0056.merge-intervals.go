c1e0966fbf85e0e39a55a716730db9c55518743a
notchips <2317853321@qq.com>
2019-08-24 23:55:50 +0800

    review

--- 1-100/0056.merge-intervals.go
+++ 1-100/0056.merge-intervals.go
@@ -4,21 +4,15 @@
  * [56] Merge Intervals
  */
 func merge(intervals [][]int) [][]int {
-	n := len(intervals)
-	if n == 0 {
+	if len(intervals) == 0 {
 		return [][]int{}
 	}
-	
 	sort.Slice(intervals, func(i, j int) bool {
-		if intervals[i][0] != intervals[j][0] {
-			return intervals[i][0] < intervals[j][0]
-		}
-		return intervals[i][1] < intervals[j][1]
+		return intervals[i][0] < intervals[j][0]
 	})
-	
-	var ans [][]int
+	ans := make([][]int, 0, 16)
 	left, right := intervals[0][0], intervals[0][1]
-	for i := 1; i < n; i++ {
+	for i := 1; i < len(intervals); i++ {
 		if intervals[i][0] > right {
 			ans = append(ans, []int{left, right})
 			left, right = intervals[i][0], intervals[i][1]
--- 1-100/0057.insert-interval.go
+++ 1-100/0057.insert-interval.go
@@ -4,51 +4,26 @@
  * [57] Insert Interval
  */
 func insert(intervals [][]int, newInterval []int) [][]int {
-	n := len(intervals)
-	if n == 0 {
-		return [][]int{newInterval}
+	if len(intervals) == 0 && len(newInterval) == 0 {
+		return [][]int{}
 	}
-	var ans [][]int
-	stage := 0
-	var left int
-	for i := 0; i < n; i++ {
-		if stage == 2 {
-			ans = append(ans, intervals[i])
-		}
-
-		if stage == 0 {
-			if intervals[i][1] < newInterval[0] {
-				ans = append(ans, intervals[i])
-			} else {
-				left = min(intervals[i][0], newInterval[0])
-				stage = 1
+	ans := make([][]int, 0, len(intervals))
+	for _, interval := range intervals {
+		// newInterval在interval的前面或者后面
+		if newInterval[1] < interval[0] || interval[1] < newInterval[0] {
+			ans = append(ans, interval)
+		} else { // newInterval和interval相交
+			if interval[0] < newInterval[0] {
+				newInterval[0] = interval[0]
 			}
-		}
-
-		if stage == 1 {
-			if newInterval[1] <= intervals[i][1] {
-				if newInterval[1] >= intervals[i][0] {
-					ans = append(ans, []int{left, intervals[i][1]})
-				} else {
-					ans = append(ans, []int{left, newInterval[1]}, intervals[i])
-				}
-				stage = 2
-			} else if i == n-1 {
-				ans = append(ans, []int{left, newInterval[1]})
+			if interval[1] > newInterval[1] {
+				newInterval[1] = interval[1]
 			}
 		}
 	}
-
-	if stage == 0 {
-		ans = append(ans, newInterval)
-	}
-
+	ans = append(ans, newInterval)
+	sort.Slice(ans, func(i, j int) bool {
+		return ans[i][0] < ans[j][0]
+	})
 	return ans
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
 }
